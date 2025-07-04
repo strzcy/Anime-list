ad3182f9286cb817dd6cc96ddc4d1409add78187
strzcy <desipuspita46832@gmail.com>
2025-07-04 13:50:49 +0700

    hihi

--- script.js
+++ script.js
@@ -3,28 +3,82 @@ const form = document.getElementById('form');
 const search = document.getElementById('search');
 
 const categories = {
+  // Watched
   "now-playing": [
     {
-      title: "Elemental",
-      overview: "Fire meets Water.",
-      poster_path: "",
+      title: "Suka Suka",
+      overview: "500 tahun manusia dikuasai oleh “Beast”. Ras yang masih bertahan hidup membuat rumah mereka mengapung dan menjadi pulau-pulau yang mengapung di langit, agar terhindar dari jangkauan Beast.",
+      poster_path: "/source/sukasuka.jpg",
+      vote_average: 7.9
+    },
+    {
+      title: "Kuma Kuma Kuma Bear",
+      overview: "Yuna, seorang gadis berumur 15-tahun, mencoba bermain permainan VRMMO yang pertama di dunia, selesai menjawab survei tentang pembaruan permainan yang terbaru, dia tiba-tiba muncul di hutan misterius.",
+      poster_path: "/source/kuma.jpg",
+      vote_average: 7.8
+    },
+    {
+      title: "Wandering Witch",
+      overview: "Wandering Witch: The Journey of Elaina ini Flanmenceritakan tentang sosok Elaina yang sejak kecil selalu terpesona dengan cerita-cerita yang ditulis dalam buku favoritnya.",
+      poster_path: "/source/witch.jpg",
+      vote_average: 8.5
+    },
+    {
+      title: "Tada Never Falls in Love",
+      overview: "Mitsuyoshi Tada adalah seorang pemuda yang tidak pernah mengenal cinta, ia bertemu dengan Teresa Wagner, seorang siswa pindahan. Tampaknya Teresa tersesat. Mitsuyoshi membantunya dan membawanya ke kedai kopi kakeknya.",
+      poster_path: "/source/tada.jpg",
       vote_average: 6.8
     },
     {
-      title: "Inside Out 2",
-      overview: "New emotions join the party.",
-      poster_path: "https://i5.walmartimages.com/asr/38c5952d-b75c-42e5-9486-6bae68fe727c_1.24b96bbc17818b921bee32b51270053e.jpeg",
-      vote_average: 7.9
-    }
-  ],
-  popular: [
+      title: "The Saint’s Magic Power Is Omnipotent",
+      overview: "Dinilai kurang cakep alias good looking, Sei Takanashi akhirnya diperlakukan kurang ajar oleh sang pangeran mahkota. Terpanggil bersama salah seorang gadis SMA berparas rupawan, Sei diperlakukan cukup berbeda di istana.",
+      poster_path: "/source/saint.jpg",
+      vote_average: 7.1
+    },
     {
-      title: "Spider-Man: No Way Home",
-      overview: "Peter Parker faces multiverse chaos.",
-      poster_path: "https://i5.walmartimages.com/asr/38c5952d-b75c-42e5-9486-6bae68fe727c_1.24b96bbc17818b921bee32b51270053e.jpeg",
-      vote_average: 8.5
-    }
+      title: "Lapis Re:LiGHTs",
+      overview: "Tiara memulai kehidupan barunya apakah kakak perempuannya menginginkannya atau tidak. Mengambil pelatih dari Bristol, dia datang ke Flora Girls 'Academy dengan impian tidak hanya bergabung dengan teman lamanya Rosetta, tetapi juga memasuki sekolah penyihir bergengsi.",
+      poster_path: "/source/lapis.jpg",
+      vote_average: 8.2
+    },
+    {
+      title: "Urara Meirochou",
+      overview: "Inilah Meiro-machi, sebuah kota tempat gadis-gadis peramal tinggal. Banyak gadis dari penjuru negeri datang ke kota ini untuk menjadi peramal yang handal (Urara). Chiya, seorang gadis dari pegunungan yang baru saja datang ke kota ini dengan tujuan lain selain menjadi Urara",
+      poster_path: "/source/urara.jpg",
+      vote_average: 9.0
+    },
+    {
+      title: "Rascal Does Not Dream of Bunny Girl",
+      overview: "emaja bernama Mai Sakurajima. Ia berpakaian seperti gadis kelinci dan berkeliling di dalam perpustakaan tanpa disadari oleh siapa pun di sana. Mai merasa heran karena Sakuta hanya satu-satunya orang yang dapat melihatnya—sementara orang lain tidak.",
+      poster_path: "/source/bunny.jpg",
+      vote_average: 6.8
+    },
+    {
+      title: "rent",
+      overview: "Fire meets Water.",
+      poster_path: "/source/rent.jpg",
+      vote_average: 6.8
+    },
+    {
+      title: "drstun",
+      overview: "Fire meets Water.",
+      poster_path: "/source/drstun.jpg",
+      vote_average: 6.8
+    },
+    {
+      title: "kiminonawa",
+      overview: "Fire meets Water.",
+      poster_path: "/source/kimino.jpg",
+      vote_average: 6.8
+    },
   ],
+
+
+
+  popular: [],
+
+
+// rekomended
   "top-rated": [
     {
       title: "The Shawshank Redemption",
@@ -41,13 +95,19 @@ const categories = {
     {
       title: "The Shawshank Redemption",
       overview: "Hope can set you free.",
-      poster_path: '/source/pasir.jpg',
+      poster_path: '/source/apotek.jpg',
       vote_average: 9.7
     },
     {
       title: "The Shawshank Redemption",
       overview: "Hope can set you free.",
-      poster_path: '/source/apotek.jpg',
+      poster_path: '/source/16bit.jpg',
+      vote_average: 9.7
+    },
+    {
+      title: "The Shawshank Redemption",
+      overview: "Hope can set you free.",
+      poster_path: '/source/pasir.jpg',
       vote_average: 9.7
     },
     {
@@ -57,6 +117,9 @@ const categories = {
       vote_average: 9.7
     }
   ],
+
+
+  // watchlist
   trending: [
     {
       title: "Deadpool & Wolverine",
